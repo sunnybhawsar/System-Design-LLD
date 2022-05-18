7231855c92323a1367a377898ad6cf01bb317662
Sunny Bhawsar <sunnybhawsar07@gmail.com>
2022-05-19 01:43:35 +0530

    Added new solution for Problem #2 - Payroll

---
+++ Machine Coding/2. Design a payroll system/Solution/PayrollConsoleApp/Payroll.ConsoleApp/src/Repositories/EmployeeRepository.cs
@@ -0,0 +1,40 @@
+﻿using Newtonsoft.Json;
+using Payroll.ConsoleApp.src.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+
+namespace Payroll.ConsoleApp.src.Repositories
+{
+    public class EmployeeRepository : IEmployeeRepository
+    {
+        private readonly string baseAddress = "http://34.198.81.140/";
+
+        public async Task<IEnumerable<Employee>> GetEmployeesAsync()
+        {
+            var employees = new List<Employee>();
+            try
+            {
+                using HttpClient httpClient = new HttpClient();
+                httpClient.BaseAddress = new Uri(baseAddress);
+                httpClient.DefaultRequestHeaders.Clear();
+                httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                var res = await httpClient.GetAsync("attendance.json");
+                if (res.IsSuccessStatusCode)
+                {
+                    employees = JsonConvert.DeserializeObject<IEnumerable<Employee>>(res.Content?.ReadAsStringAsync()?.Result)?.ToList();
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+
+            return employees;
+        }
+    }
+}
---
+++ Machine Coding/2. Design a payroll system/Solution/PayrollConsoleApp/Payroll.ConsoleApp/src/Repositories/IEmployeeRepository.cs
@@ -0,0 +1,11 @@
+﻿using Payroll.ConsoleApp.src.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Payroll.ConsoleApp.src.Repositories
+{
+    public interface IEmployeeRepository
+    {
+        Task<IEnumerable<Employee>> GetEmployeesAsync();
+    }
+}
